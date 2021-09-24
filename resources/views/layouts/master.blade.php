ac1a0688c364d170b8768766992ea891942d0580
FeryPermana <muhammad.permana@students.amikom.ac.id>
2021-09-24 22:16:56 +0700

    no

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>{{ config('app.name') }} | @yield('title')</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+   <!-- Bootstrap 3.3.7 -->
+    <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="{{ asset('bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{ asset('bower_components/font-awesome/css/font-awesome.min.css') }}">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="{{ asset('bower_components/Ionicons/css/ionicons.min.css') }}">
+  <!-- jvectormap -->
+  <link rel="stylesheet" href="{{ asset('bower_components/jvectormap/jquery-jvectormap.css') }}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ asset('dist/css/AdminLTE.min.css') }}">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="{{ asset('dist/css/skins/_all-skins.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css') }}">
+
+
+    <!-- Google Font -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+
+<body class="hold-transition sidebar-mini skin-purple-light">
+    <!-- Site wrapper -->
+    <div class="wrapper">
+
+        @includeIf('layouts.navbar')
+
+        <!-- =============================================== -->
+
+        @includeIf('layouts.sidebar')
+
+        <!-- =============================================== -->
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <h1>
+                   @yield('title')
+                </h1>
+                <ol class="breadcrumb">
+                    @section('breadcrumb')
+                    <li><a href="{{ url('/') }}"><i class="fa fa-dashboard"></i> Home</a></li>
+                    @show
+                </ol>
+            </section>
+
+            <!-- Main content -->
+            <section class="content">
+                @yield('content')
+            </section>
+        </div>
+    </div>
+    <div class="box-footer">
+        Footer
+    </div>
+    <!-- /.box-footer-->
+    </div>
+    <!-- /.box -->
+
+    </section>
+    <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+    @includeIf('layouts.footer')
+
+    <!-- Moment -->
+    <script src="{{ asset('bower_components/moment/min/moment.min.js') }}"></script>
+
+    <!-- DataTables -->
+    {{-- <script src="{{ asset('bower_components/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js') }}"></script> --}}
+    <!-- AdminLTE App -->
+    <!-- jQuery 3 -->
+    <script src="{{ asset('bower_components/jquery/dist/jquery.min.js') }}"></script>
+    <!-- Bootstrap 3.3.7 -->
+    <script src="{{ asset('bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    <!-- FastClick -->
+    <script src="{{ asset('bower_components/fastclick/lib/fastclick.js') }}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{ asset('dist/js/adminlte.min.js') }}"></script>
+    <!-- Sparkline -->
+    <script src="{{ asset('bower_components/jquery-sparkline/dist/jquery.sparkline.min.js') }}"></script>
+    <!-- jvectormap  -->
+    <script src="{{ asset('plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
+    <script src="{{ asset('plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
+     <!-- DataTables -->
+     <script src="{{ asset('bower_components/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+     <script src="{{ asset('bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js') }}"></script>
+    <!-- SlimScroll -->
+    <script src="{{ asset('bower_components/jquery-slimscroll/jquery.slimscroll.min.js') }}"></script>
+    <!-- ChartJS -->
+    <script src="{{ asset('bower_components/chart.js/Chart.js') }}"></script>
+    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+    <script src="{{ asset('dist/js/pages/dashboard2.js') }}"></script>
+    <script src="{{ asset('js/validator.min.js') }}"></script>
+    <script src="{{ asset('js/sweetalert.min.js') }}"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="{{ asset('dist/js/demo.js') }}"></script>
+    <script>
+        function preview(selector, temporaryFile, width = 200)  {
+            $(selector).empty();
+            $(selector).append(`<img src="${window.URL.createObjectURL(temporaryFile)}" width="${width}">`);
+        }
+    </script>
+    @stack('scripts')
+    @include('sweetalert::alert')
+
+</body>
+
+</html>
---
+++ resources/views/produk/index.blade.php
@@ -0,0 +1,205 @@
+@extends('layouts.master')
+@section('title')
+    Produk
+@endsection
+
+@section('breadcrumb')
+    @parent
+    <li class="active">produk</li>
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box">
+                <div class="box-header with-border">
+                    <button onclick="addForm('{{ route('produk.store') }}')" class="btn btn-success btn-flat"><i
+                            class="fa fa-plus-circle"></i> Tambah</button>
+                </div>
+                <div class="box-body table-responsive">
+                    <table id="tabel" class="table table-stiped table-bordered">
+                        <thead>
+                            <th width="5%">No</th>
+                            <th>Kode</th>
+                            <th>Nama</th>
+                            <th>Kategori</th>
+                            <th>Merk</th>
+                            <th>Harga Beli</th>
+                            <th>Harga Jual</th>
+                            <th>Diskon</th>
+                            <th>Stok</th>
+                            <th width="15%"><i class="fa fa-cog"></i>Aksi</th>
+                        </thead>
+                        <tbody></tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+@includeIf('produk.form')
+@push('scripts')
+    <script>
+        let table;
+
+        $(document).ready(function() {
+            table = $('#tabel').DataTable({
+                processing: true,
+                autoWidth: false,
+                ajax: {
+                    url: "{{ route('produk.data') }}",
+                },
+                columns: [{
+                        data: 'DT_RowIndex',
+                        searchable: false,
+                        sortable: false
+                    },
+                    {
+                        data: 'kode_produk'
+                    },
+                    {
+                        data: 'nama_produk'
+                    },
+                    {
+                        data: 'nama_kategori'
+                    },
+                    {
+                        data: 'merk'
+                    },
+                    {
+                        data: 'harga_beli'
+                    },
+                    {
+                        data: 'harga_jual'
+                    },
+                    {
+                        data: 'diskon'
+                    },
+                    {
+                        data: 'stock'
+                    },
+                    {
+                        data: 'aksi',
+                        searchable: false,
+                        sortable: false
+                    },
+                ]
+            });
+
+            $("#modal-form").validator().on('submit', function(e) {
+                if (!e.preventDefault()) {
+                    $.post($('#modal-form form').attr('action'), $('#modal-form form').serialize())
+                        .done((response) => {
+                            swal({
+                                type: "success",
+                                icon: "success",
+                                title: "BERHASIL!",
+                                text: response,
+                                timer: 1500,
+                                showConfirmButton: false,
+                                showCancelButton: false,
+                                buttons: false,
+                                });
+                            $('#modal-form').modal('hide');
+                            table.ajax.reload();
+                        })
+                        .fail((errors) => {
+                            alert('Tidak dapat menyimpan data');
+                            return
+                        })
+                }
+            })
+        });
+
+        function addForm(url) {
+            $('#modal-form').modal('show');
+            $('#modal-form .modal-title').text('Tambah Produk');
+
+            $('#modal-form form')[0].reset();
+            $('#modal-form form').attr('action', url);
+            $('#modal-form [name=_method]').val('post');
+            $('#modal-form [name=nama_produk]').focus();
+        }
+
+        function editForm(url) {
+            $('#modal-form').modal('show');
+            $('#modal-form .modal-title').text('Edit Produk');
+
+            $('#modal-form form')[0].reset();
+            $('#modal-form form').attr('action', url);
+            $('#modal-form [name=_method]').val('put');
+            $('#modal-form [name=nama_produk]').focus();
+            // url show sama url update itu sama
+            $.get(url)
+                .done((response) => {
+                    $('#modal-form [name=nama_produk]').val(response.nama_produk);
+                    $('#modal-form [name=id_kategori]').val(response.id_kategori);
+                    $('#modal-form [name=merk]').val(response.merk);
+                    $('#modal-form [name=harga_beli]').val(response.harga_beli);
+                    $('#modal-form [name=harga_jual]').val(response.harga_jual);
+                    $('#modal-form [name=diskon]').val(response.diskon);
+                    $('#modal-form [name=stock]').val(response.stock);
+                })
+                .fail((errors) => {
+                    alert('Tidak dapat menampilkan data');
+                    return;
+                })
+        }
+
+        function deleteData(id) {
+            var id = id;
+            var token = $("meta[name='csrf-token']").attr("content");
+            swal({
+                title: "APAKAH KAMU YAKIN ?",
+                text: "INGIN MENGHAPUS PRODUK INI!",
+                icon: "warning",
+                buttons: [
+                    'TIDAK',
+                    'YA'
+                ],
+                dangerMode: true,
+            }).then(function(isConfirm) {
+                if (isConfirm) {
+                    //ajax delete
+                    jQuery.ajax({
+                        url: "{{ route('produk.index') }}/" + id,
+                        data: {
+                            "id": id,
+                            "_token": token
+                        },
+                        type: 'DELETE',
+                        success: function(response) {
+                            if (response.status == "success") {
+                                swal({
+                                    title: 'BERHASIL!',
+                                    text: 'PRODUK BERHASIL DIHAPUS!',
+                                    icon: 'success',
+                                    timer: 1000,
+                                    showConfirmButton: false,
+                                    showCancelButton: false,
+                                    buttons: false,
+                                }).then(function() {
+                                    location.reload();
+                                });
+                            } else {
+                                swal({
+                                    title: 'GAGAL!',
+                                    text: 'PRODUK GAGAL DIHAPUS!',
+                                    icon: 'error',
+                                    timer: 1000,
+                                    showConfirmButton: false,
+                                    showCancelButton: false,
+                                    buttons: false,
+                                }).then(function() {
+                                    location.reload();
+                                });
+                            }
+                        }
+                    });
+                } else {
+                    return true;
+                }
+            })
+        }
+    </script>
+@endpush
